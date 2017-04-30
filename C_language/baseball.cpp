670f7e9ff895ca4bfbe27482fb94be29d2a5a0d5
bienseo <bienseo04@gmail.com>
2017-05-01 00:09:08 +0900

    숫자야구과제
    숫자야구과제 수정

--- C_language/baseball.cpp
+++ C_language/baseball.cpp
@@ -1,75 +1,77 @@
+/*
+ 숫자야구과제
+ */
 #include<iostream>
 #include<time.h>
 using namespace std;
 
 int main()
 {
-	int ans[3];
-	int guess[3];
-	int strike, ball, num=0;
-	int i;
-	int score;
-
-	srand(time(NULL));
-	
-	while (true)
-	{
-		for (i = 0; i < 3; i++)
-		{
-			ans[i] = rand() % 10;
-		}
-		if (ans[0] != ans[1] && ans[0] != ans[2] && ans[1] != ans[2]) break;
-	}
-
-	cout << "숫자야구를 시작해봅시다."<<endl;
-
-	while(true)
-	{
-		strike = ball = 0;
-		cout << "\n 0에서 9사이의 숫자 3개를 생각해서 입력해주세요 \n"<<endl;
-		
-		for (i = 0; i < 3; i++)
-		{
-			cin >> guess[i];
-		}
-		
-		if (ans[0] == guess[0])
-		{
-			strike++;
-		}
-		else if (ans[0] == guess[1] || ans[0] == guess[2])
-		{
-			ball++;
-		}
-
-		if (ans[1] == guess[1])
-		{
-			strike++;
-		}
-		else if (ans[1] == guess[0] || ans[1] == guess[2])
-		{
-			ball++;
-		}
-
-		if (ans[2] == guess[2])
-		{
-			strike++;
-		}
-		else if (ans[2] == guess[0] || ans[2] == guess[1])
-		{
-			ball++;
-		}
-		++num;
-		cout << "\n";
-		cout << strike << " strike " << ball << " ball " << num<<" 번\n"<<endl;
-
-		score = (101 - num) * 50;
-
-		if (strike == 3) {
-			cout << "횟수: " << num <<" 점수: "<<score<<"입니다.\n"<<endl;
-			break;
-		}
-	}
-
-	return 0;
+    int ans[3];
+    int guess[3];
+    int strike, ball, num=0;
+    int i;
+    int score;
+    
+    srand(time(NULL));
+    
+    while (true)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            ans[i] = rand() % 10;
+        }
+        if (ans[0] != ans[1] && ans[0] != ans[2] && ans[1] != ans[2]) break;
+    }
+    
+    
+    while(true)
+    {
+        strike = ball = 0;
+        cout << "숫자야구 게임!\n제가 생각하고 있는 세 자리수를 맞춰보세용~ 최고점은 5000점 입니다 ^.^* \n(*한자리씩 입력해주세요.) \n"<<endl;
+        
+        for (i = 0; i < 3; i++)
+        {
+            cin >> guess[i];
+        }
+        
+        if (ans[0] == guess[0])
+        {
+            strike++;
+        }
+        else if (ans[0] == guess[1] || ans[0] == guess[2])
+        {
+            ball++;
+        }
+        
+        if (ans[1] == guess[1])
+        {
+            strike++;
+        }
+        else if (ans[1] == guess[0] || ans[1] == guess[2])
+        {
+            ball++;
+        }
+        
+        if (ans[2] == guess[2])
+        {
+            strike++;
+        }
+        else if (ans[2] == guess[0] || ans[2] == guess[1])
+        {
+            ball++;
+        }
+        ++num;
+        cout << "\n";
+        cout << strike << " strike " << ball << " ball " << num<<" 회 \n"<<endl;
+        
+        score = (101 - num) * 50;
+        
+        if (strike == 3) {
+            cout << "정답입니다! " << num <<" 회 만에 맞추신 당신의 점수는 "<<score<<" 입니다."<<endl;
+            break;
+        }
+    }
+    
+    return 0;
 }
