47939f22132eca348d671b2f09f29399ef548728
bienseo <bienseo04@gmail.com>
2017-05-11 11:30:12 +0900

    Display target using python
    Display target using python

---
+++ project_hanzo/dotmat.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+target = np.array([[1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,1,1,0,0,1],[1,0,0,1,1,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1]])
+
+plt.imshow(target)
+plt.show()
